e47cad2d54739be62c3049ed437c434fbcf9c4cf
chenxianda-3 <50940514+chenxianda-3@users.noreply.github.com>
2020-07-09 11:31:29 +0800

    Update TestConflict.java

--- test/src/com/test/ws/client/TestConflict.java
+++ test/src/com/test/ws/client/TestConflict.java
@@ -4,5 +4,6 @@ public class TestConflict {
 	public static void main(String[] args) {
 		System.out.println("first conflict");
 		System.out.println("third conflict");
+		System.out.println("forth conflict");
 	}
 }
