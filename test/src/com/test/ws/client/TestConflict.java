4844ab54c5891c997d255d43b8461abfd2c7f759
chenxianda-3 <50940514+chenxianda-3@users.noreply.github.com>
2020-07-09 11:50:34 +0800

    trim

--- test/src/com/test/ws/client/TestConflict.java
+++ test/src/com/test/ws/client/TestConflict.java
@@ -8,8 +8,6 @@ public class TestConflict {
 		System.out.println("fifth conflict");
 		System.out.println("sixth conflict");
 		System.out.println("tenth conflict");
-		
-		
 		System.out.println("seven conflict");
 		System.out.println("eight conflict");
 		System.out.println("nighth conflict");
