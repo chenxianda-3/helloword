2b96c132a5939f34e3d80dd5ba8e02711f9aded1
chenxianda <chenxianda@digibms.com>
2020-07-15 07:53:25 +0800

    26

--- test/src/com/test/ws/client/TestConflict.java
+++ test/src/com/test/ws/client/TestConflict.java
@@ -8,5 +8,6 @@ public class TestConflict {
 		System.out.println("23 conflict");
 		System.out.println("24 conflict");
         System.out.println("25 conflict");
+		System.out.println("26 conflict");
 	}
 }
