4cae9c66c9ba08cde60e650809d74468a924de34
chenxianda <chenxianda@digibms.com>
2020-07-14 17:53:06 +0800

    20

--- test/src/com/test/ws/client/TestConflict.java
+++ test/src/com/test/ws/client/TestConflict.java
@@ -2,20 +2,7 @@ package com.test.ws.client;
 
 public class TestConflict {
 	public static void main(String[] args) {
-		System.out.println("first conflict");
-		System.out.println("third conflict");
-		System.out.println("forth conflict");
-		System.out.println("fifth conflict");
-		System.out.println("sixth conflict");
-		System.out.println("tenth conflict");
-		System.out.println("seven conflict");
-		System.out.println("eight conflict");
-		System.out.println("nighth conflict");
-		System.out.println("eleven conflict");
-		System.out.println("twtheven conflict");
-        System.out.println("thirdth conflict");
-        System.out.println("fifth conflict222");
-		System.out.println("fourteen conflict");
+		System.out.println("20 conflict");
 
 	}
 }
