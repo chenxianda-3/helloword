1ee105347358a1d11cde47c4311066275f2c8443
chenxianda-3 <50940514+chenxianda-3@users.noreply.github.com>
2020-07-15 07:25:48 +0800

    21

--- test/src/com/test/ws/client/TestConflict.java
+++ test/src/com/test/ws/client/TestConflict.java
@@ -3,6 +3,6 @@ package com.test.ws.client;
 public class TestConflict {
 	public static void main(String[] args) {
 		System.out.println("20 conflict");
-
+		System.out.println("21 conflict");
 	}
 }
