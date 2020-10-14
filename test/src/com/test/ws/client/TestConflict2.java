8024f6a84356081df8f289547a0d6616b93a130d
chenxianda <chenxianda@digibms.com>
2020-10-14 10:03:14 +0800

    31 commit

---
+++ test/src/com/test/ws/client/TestConflict2.java
@@ -0,0 +1,18 @@
+package com.test.ws.client;
+
+public class TestConflict2 {
+	public static void main(String[] args) {
+		System.out.println("20 conflict");
+		System.out.println("21 conflict");
+		System.out.println("22 conflict");
+		System.out.println("23 conflict");
+		System.out.println("24 conflict");
+        System.out.println("25 conflict");
+		System.out.println("26 conflict");
+		System.out.println("27 conflict");
+		System.out.println("28 conflict");
+		System.out.println("29 conflict");
+		System.out.println("30 conflict");
+		System.out.println("31 conflict");
+	}
+}
