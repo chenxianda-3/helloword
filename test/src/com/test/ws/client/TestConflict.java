f5f282c568fb0d502d7a3b04541274e4baf1db40
chenxianda-3 <50940514+chenxianda-3@users.noreply.github.com>
2020-10-14 09:53:20 +0800

    30 commit

--- test/src/com/test/ws/client/TestConflict.java
+++ test/src/com/test/ws/client/TestConflict.java
@@ -12,5 +12,6 @@ public class TestConflict {
 		System.out.println("27 conflict");
 		System.out.println("28 conflict");
 		System.out.println("29 conflict");
+		System.out.println("30 conflict");
 	}
 }
