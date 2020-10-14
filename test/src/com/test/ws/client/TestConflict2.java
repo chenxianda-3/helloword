2c8f3f343a2283a840e6fc3be28e1e24de37eea7
chenxianda <chenxianda@digibms.com>
2020-10-14 15:14:59 +0800

    test merge 29

--- test/src/com/test/ws/client/TestConflict2.java
+++ test/src/com/test/ws/client/TestConflict2.java
@@ -10,5 +10,6 @@ public class TestConflict2 {
 		System.out.println("26 conflict");
 		System.out.println("27 conflict");
 		System.out.println("28 conflict");
+		System.out.println("29 conflict");
 	}
 }
